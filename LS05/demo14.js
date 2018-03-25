e43d94fb7b7d02dd4ec3fa7735fb829cfa76c48e
weijia0208 <1002475276@qq.com>
2018-03-25 20:16:10 +0800

    第五次课更新

---
+++ LS05/demo14.js
@@ -0,0 +1,9 @@
+console.log(2>1&&4<5);     //true
+console.log(true&&(!2));   //false
+console.log(false&&("2" == 2));   //false
+console.log(false&&false);    //false
+
+console.log(2>1||4<5);    //true
+console.log(true||(!2));  //true
+console.log(false||("2" == 2));   //true
+console.log(false||false);   //false
